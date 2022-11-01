243fc3b5eecb506bd69caaee4c7a6bbc9f84fe5e
Is0meone <stkwiat22a@gmail.com>
2022-11-01 19:58:20 +0100

    Utworzenie pliku

---
+++ Palindrome.java
@@ -0,0 +1,17 @@
+public class Palindrome {
+    public static void main (String[] arguments) {
+        String word = "abccba";
+        String longestPalindrome = findLongestPalindrome(word);
+    }
+    public static String findLongestPalindrome(String word) {
+        for (int i = 0; i < word.length(); i++) {
+            /* Write your code here */
+        }
+    }
+    public static String analyzeSubstring(String word) {
+        /* Write your code here */
+    }
+    public static boolean isPalindrome(String word) {
+        /* Write your code here */
+    }
+}
